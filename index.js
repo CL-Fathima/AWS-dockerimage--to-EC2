d651f1933efa8f7699c546423940ac0e3f046d78
CL-Fathima <122019348+CL-Fathima@users.noreply.github.com>
2023-04-03 14:56:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("welcome to express app!!");
+  res.send("HELLO, welcome to express app!!");
 });
 
 app.get("/info", (req, res) => {
